ea318f5c1f4839e0b6ea9263738b5c544d95c2ac
Lewechi Godsfavour <lewechigodsfavour@gmail.com>
2024-05-30 23:38:35 +0100

    Initial commit

---
+++ Frame/FrameApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrameApp.swift
+//  Frame
+//
+//  Created by Lewechi Godsfavour on 30/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct FrameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
